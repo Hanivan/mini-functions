5c915e88c99e9b0c4e91032ecb765e680c164151
Hanivan <hanivan20@gmail.com>
2021-11-07 19:39:29 +0700

    add isDivisible function

---
+++ C++/isDivisible_8k.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+bool isDivisible(int n, int x, int y) {
+  return (n % x == 0 && n % y == 0) ? true : false;
+}
+
+int main() {
+
+  int n = 3, x = 3, y = 4;
+
+  std::cout << isDivisible(n, x, y);
+
+  return 0;
+}
